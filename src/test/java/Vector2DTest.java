d706bdf0e14e7fcb88ebf038274dbf802e022adf
Mykhailo Klym <epifaniusosbm@gmail.com>
2021-01-30 05:19:50 -0700

    Upgrade JUnit version to 5

--- src/test/java/Vector2DTest.java
+++ src/test/java/Vector2DTest.java
@@ -1,11 +1,11 @@
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class Vector2DTest {
 
     @Test
     public void newVectorShouldHaveZeroLength(){
         Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0, v1.length(), 1e-9);
+        Assertions.assertEquals(0, v1.length(), 1e-9);
     }
 }
